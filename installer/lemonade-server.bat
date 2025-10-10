182ec338854eb80838318bf28fff95d4cfae68f9
Tetramatrix <chibox@gmail.com>
2025-10-10 11:10:11 +0200

    Update lemonade-server.bat

--- installer/lemonade-server.bat
+++ installer/lemonade-server.bat
@@ -9,11 +9,15 @@ for /f "tokens=1-4 delims=:.," %%a in ("!time!") do (
 REM Use temp directory for the lock file
 set "LOCK_FILE=%TEMP%\lemonade_server.lock"
 
+REM Default to "serve" if no arguments are provided
+set "CLI_ARGS=%*"
+if "%CLI_ARGS%"=="" set "CLI_ARGS=serve"
+
 REM Show a notification and run the server in tray mode.
 REM Note: command line arguments are parsed in order from left to right
 set TRAY=0
 set ARGS=
-for %%a in (%*) do (
+for %%a in (!CLI_ARGS!) do (
     set ARGS=!ARGS! %%a
     if /I "%%a"=="serve" (
         set TRAY=1
