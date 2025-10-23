a5d2fc882c7c1269fd9b76819b9c18082a075a9b
Jeremy Fowers <jeremy.fowers@amd.com>
2025-10-23 17:14:12 -0400

    minor fixes for flm

--- test/server_llamacpp.py
+++ test/server_llamacpp.py
@@ -102,7 +102,11 @@ class LlamaCppTesting(ServerTestingBase):
         complete_response = ""
         chunk_count = 0
         for chunk in stream:
-            if chunk.choices and chunk.choices[0].delta.content is not None:
+            if (
+                chunk.choices
+                and chunk.choices[0].delta
+                and chunk.choices[0].delta.content is not None
+            ):
                 complete_response += chunk.choices[0].delta.content
                 print(chunk.choices[0].delta.content, end="")
                 chunk_count += 1
