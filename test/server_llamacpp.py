f16306abf43002adf34ae5fb601a26a04712c394
Jeremy Fowers <jeremy.fowers@amd.com>
2025-10-28 13:43:29 -0400

    ci bugs

--- test/server_llamacpp.py
+++ test/server_llamacpp.py
@@ -67,6 +67,10 @@ class LlamaCppTesting(ServerTestingBase):
         if self.llamacpp_backend != "rocm":
             return
 
+        # Skip when testing C++ server (this is Python-specific functionality)
+        if is_cpp_server():
+            self.skipTest("Skipping Python-specific test when using C++ server")
+
         # FIXME: Our HIP ID detection works in most machines, but seems to specifically fail on
         # our CI Linux machines. We should investigate why this is happening.
         # https://github.com/lemonade-sdk/lemonade/issues/272
