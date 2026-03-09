d919917e85f2c9fe86c52fe9dbb57bc1588f18b8
Ksenia Lebedeva <ksenia@bitgamma.com>
2026-03-09 14:57:20 +0100

    transformed voice select into combobox

---
+++ src/app/src/renderer/components/Combobox.tsx
@@ -0,0 +1,50 @@
+import React from 'react';
+
+interface ComboboxProps {
+  optionsList: any[];
+  onChangeFunc: (e: any) => void;
+  defaultValue: string;
+}
+
+const Combobox: React.FC<ComboboxProps> = React.memo(function Combobox({ optionsList, onChangeFunc, defaultValue }) {
+  const [datalistOpen, setDatalistlistOpen] = React.useState<boolean>(false);
+  const [filteredList, setFilteredList] = React.useState<string[]>(optionsList);
+
+  const updateInputFromList = (e: any) => {
+    let opt = e.target.dataset.option;
+    if (opt) onChangeFunc(opt);
+    setDatalistlistOpen(!datalistOpen);
+  }
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (!datalistOpen) setDatalistlistOpen(true);
+    let newVoiceList = e.target.value == '' ? optionsList : filteredList.filter((word) => word.startsWith(e.target.value));
+    onChangeFunc(e.target.value);
+    setFilteredList(newVoiceList);
+  }
+
+  return (
+    <div className="form-combobox-container">
+      <input
+        className="form-combobox"
+        value={defaultValue}
+        onChange={handleInputChange}
+        onClick={() => setDatalistlistOpen(!datalistOpen)}
+        list='voiceOptionsList'
+        placeholder='Select a voice...'
+      />
+      {
+        datalistOpen && (filteredList.length > 0) && (
+          <ul id="voiceOptionsList" className="form-datalist">
+            {filteredList.sort().map((voice: string, index: number) => {
+              return <li key={index} data-option={voice} onClick={updateInputFromList} className="form-datalist-option">{voice}</li>;
+            })}
+          </ul>)
+      }
+    </div>
+  )
+}, (prevProps, nextProps) => {
+  return ((nextProps.defaultValue === prevProps.defaultValue));
+});
+
+export default Combobox;
