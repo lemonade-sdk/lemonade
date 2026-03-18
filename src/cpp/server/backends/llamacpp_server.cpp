ea1f372518cc93c9b25316046e1eba8567571405
Adam <adam@kisara>
2026-03-18 15:05:02 +0200

    Checked for empty

--- src/cpp/server/backends/llamacpp_server.cpp
+++ src/cpp/server/backends/llamacpp_server.cpp
@@ -201,7 +201,9 @@ void LlamaCppServer::load(const std::string& model_name,
 
     push_arg(args, reserved_flags, "-m", gguf_path, std::vector<std::string>{"--model"});
     push_arg(args, reserved_flags, "--ctx-size", std::to_string(ctx_size), std::vector<std::string>{"-c"});
-    push_arg(args, reserved_flags, "--device", backend_device);
+    if (backend_device != "") {
+      push_arg(args, reserved_flags, "--device", backend_device);
+    }
     push_arg(args, reserved_flags, "--port", std::to_string(port_));
     push_arg(args, reserved_flags, "--jinja", std::vector<std::string>{"--no-jinja"});
 
