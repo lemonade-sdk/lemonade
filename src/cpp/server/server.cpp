ec5e001ad80434b2696be785b235eca955687871
Arun Babu Neelicattu <arun.neelicattu@gmail.com>
2026-06-26 15:39:56 +0200

    feat(server): expose model type, device, and source properties in model list JSON API

--- src/cpp/server/server.cpp
+++ src/cpp/server/server.cpp
@@ -1,6 +1,7 @@
 #include "lemon/server.h"
 #include "lemon/error_types.h"
 #include <optional>
+#include "lemon/canonical_id.h"
 #include "lemon/collection_orchestrator.h"
 #include "lemon/hf_variants.h"
 #include "lemon/model_registry.h"
@@ -2818,6 +2819,22 @@ nlohmann::json Server::model_info_to_json(const std::string& model_id, const Mod
     for (const auto& component : info.components) {
         public_components.push_back(model_manager_->get_public_model_name(component));
     }
+    std::string source_str = "builtin";
+    if (info.recipe == "cloud") {
+        source_str = "cloud";
+    } else {
+        std::string canonical_name = model_manager_->resolve_model_name(model_id);
+        if (auto canon = parse_canonical_id(canonical_name)) {
+            if (canon->source == ModelSource::Registered) {
+                source_str = "user";
+            } else if (canon->source == ModelSource::Imported) {
+                source_str = "extra";
+            } else if (canon->source == ModelSource::Builtin) {
+                source_str = "builtin";
+            }
+        }
+    }
+
     nlohmann::json model_json = {
         {"id", model_id},
         {"object", "model"},
@@ -2834,6 +2851,9 @@ nlohmann::json Server::model_info_to_json(const std::string& model_id, const Mod
         {"labels", info.labels},
         {"components", public_components},
         {"recipe_options", info.recipe_options.to_json()},
+        {"type", model_type_to_string(info.type)},
+        {"device", device_type_to_string(info.device)},
+        {"source", source_str},
     };
 
     // Surface the cloud provider on cloud entries so the Model Manager can
